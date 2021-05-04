d491fae9c1b6f6a70a2b38b5c42afb05ff21fa54
Dan Bumbarger <danb@alleninstitute.org>
2021-05-04 15:00:41 -0700

    methods now return meshset

--- meshlab_cleanup.py
+++ meshlab_cleanup.py
@@ -2,6 +2,7 @@
 
 import pymeshlab
 import numpy as np
+import time #temp for diagnosis
 
 def make_watertight(meshset, ambient_occlusion = True, fix_zero_area_faces = True):
     '''
@@ -16,28 +17,28 @@ def make_watertight(meshset, ambient_occlusion = True, fix_zero_area_faces = Tru
     #I was merging close vertices and removing null faces, which caused problems in that it creates zero area holes, which are hard to fix. 
 
     # meshset.turn_into_a_pure_triangular_mesh() 
-    # meshset.remove_duplicate_vertices()
+    meshset.remove_duplicate_vertices()
     meshset.remove_duplicate_faces() 
+    meshset.merge_close_vertices()
     meshset.remove_unreferenced_vertices()
     meshset.current_mesh().compact()
+    print_checks(meshset)#for debugging
+    meshset = fix_non_manifold(meshset)
 
-    fix_non_manifold(meshset)
-
+    #####I MAY WANT TO CHANGE THIS TO VOLUMETRIC OBSCURANCE#####
     if ambient_occlusion:
         print('Removing vertices through ambient occlusion')
         try:
-            vertex_removal_ambient_occlusion(meshset)
+            meshset = vertex_removal_ambient_occlusion(meshset)
             meshset.close_holes(maxholesize = 150, selfintersection = False, newfaceselected = False)
-            print('Checking non manifold again')
-            fix_non_manifold(meshset)
-            print('moving on')
+            meshset = fix_non_manifold(meshset)
         except:
             print('Ambient occlusion removal failed')
 
     if fix_zero_area_faces:
         print('fixing zero area faces')
         while True:
-            if zero_area_faces(meshset) > 0:
+            if zero_area_face_number(meshset) > 0:
                 #Failing here sometimes? Need a try block?864691136084203884 fails
                 #864691136084203884 would work prior to ambient occlusion
                 #crashes the whole kernel
@@ -45,32 +46,34 @@ def make_watertight(meshset, ambient_occlusion = True, fix_zero_area_faces = Tru
                 #Is it a memory thing, and if I go slow enough it doesn't crash?
                 print('Fixing zero faces by edge flipping')
                 meshset.remove_t_vertices_by_edge_flip()
-                fix_non_manifold(meshset)
-
-                if zero_area_faces(meshset) > 0:
+                print('Fixing non-manifold')
+                meshset = fix_non_manifold(meshset)
+                print_checks(meshset)#for debugging
+                if zero_area_face_number(meshset) > 0:
                     print('Fixing zero faces by edge collapse')
                     meshset.remove_t_vertices_by_edge_collapse()
-                    fix_non_manifold(meshset)
+                    print_checks(meshset)#for debugging
+                    meshset = fix_non_manifold(meshset)
 
-                    if zero_area_faces(meshset) > 0:
+                    if zero_area_face_number(meshset) > 0:
                         print('Fixing zero faces by isotropic explicit remeshing')
                         meshset.remove_zero_area_faces()
                         meshset.select_none()
                         meshset.select_border()
                         meshset.dilate_selection()
                         meshset.dilate_selection()
-                        fix_non_manifold(meshset)
+                        meshset = fix_non_manifold(meshset)
 
-                        if zero_area_faces(meshset) > 0:
+                        if zero_area_face_number(meshset) > 0:
                             print('Fixing zero faces vertex deletion')
                             meshset.remove_zero_area_faces()
                             meshset.select_none()
                             meshset.select_border()
                             meshset.delete_selected_vertices()
-                            fix_non_manifold(meshset)
+                            meshset = fix_non_manifold(meshset)
 
-            delete_small_disconnected_component(meshset)
-            if zero_area_faces(meshset) == 0:
+            meshset = delete_small_disconnected_component(meshset)
+            if zero_area_face_number(meshset) == 0:
                 ('Successfully repaired zero area faces')
                 break
     meshset.current_mesh().compact()
@@ -83,6 +86,7 @@ def delete_small_disconnected_component(meshset):
     meshset.delete_selected_faces()
     meshset.delete_selected_vertices()
     meshset.remove_unreferenced_vertices()
+    return meshset
 
 def vertex_removal_ambient_occlusion(meshset):
     meshset.current_mesh().compact()
@@ -95,10 +99,11 @@ def vertex_removal_ambient_occlusion(meshset):
         print(f'Removing {meshset.current_mesh().selected_vertex_number()} vertices in vertex_removal_ambient_occlusion')
         meshset.delete_selected_faces()
         meshset.delete_selected_vertices()
-        delete_small_disconnected_component(meshset)
+        meshset = delete_small_disconnected_component(meshset)
         meshset.current_mesh().compact()
     else:
         print('No vertices to remove in vertex_removal_ambient_occlusion')
+    return meshset
 
 def is_2_manifold(meshset):
     meshset.select_non_manifold_edges_()
@@ -109,15 +114,31 @@ def is_2_manifold(meshset):
         return False
     return True
 
-def zero_area_faces(meshset):
+def zero_area_face_number(meshset):
     meshset.current_mesh().compact()
     meshset.select_none()
     meshset.per_face_quality_according_to_triangle_shape_and_aspect_ratio(metric = 'Area')
     face_quality = meshset.current_mesh().face_quality_array()#returns numpy array
     num_zero_area = face_quality.size - np.count_nonzero(face_quality)
-    print(f'{num_zero_area} zero area faces found.')
     return num_zero_area
 
+def self_intersecting_face_number(meshset):
+    meshset.current_mesh().compact()
+    meshset.select_none()
+    meshset.select_self_intersecting_faces()
+    return meshset.current_mesh().selected_face_number()
+
+def problematic_face_number(meshset):
+    meshset.current_mesh().compact()
+    meshset.select_none()
+    meshset.select_problematic_faces()
+    return meshset.current_mesh().selected_face_number()
+
+def border_vertex_number(meshset):
+    meshset.current_mesh().compact()
+    meshset.select_none()
+    meshset.select_border()
+    return meshset.current_mesh().selected_vertex_number()
 
 def fix_non_manifold(meshset):
     if not is_2_manifold(meshset):
@@ -151,6 +172,16 @@ def fix_non_manifold(meshset):
 
             if is_2_manifold(meshset):
                break
+    return meshset
+
+def print_checks(meshset):
+    print("__________________________________________")
+    print(f'Is 2-manifold?:{is_2_manifold(meshset)}')
+    print(f'# zero area faces:{zero_area_face_number(meshset)}')
+    print(f'Self intersecting faces:{self_intersecting_face_number(meshset)}')
+    print(f'Problematic faces:{problematic_face_number(meshset)}')
+    print(f'Border vertices:{border_vertex_number(meshset)}')
+    print("__________________________________________")
 
 def fix_self_intersecting(meshset):
     #This needs work! Don't trust it!
@@ -173,12 +204,12 @@ def fix_self_intersecting(meshset):
             meshset.delete_selected_vertices()
             meshset.remove_zero_area_faces()
             meshset.remove_unreferenced_vertices()
-            delete_small_disconnected_component(meshset)
+            meshset = delete_small_disconnected_component(meshset)
 
             meshset.select_border()
             meshset.close_holes(maxholesize = 150, selfintersection = False, newfaceselected = False)
     
-    return start_verts - meshset.current_mesh().vertex_number()
+    return meshset, start_verts - meshset.current_mesh().vertex_number()
 
     #meshset.simplification_quadric_edge_collapse_decimation(targetperc = 0.2, preserveboundary = True, preservetopology = True)
 
